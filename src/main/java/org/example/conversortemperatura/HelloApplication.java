f514a4c3f71aef98b9a3f31d231e8c327ea78f99
ivan baguena <baguena.delabandera.ivan@gmail.com>
2024-10-14 23:46:07 +0200

    terminado

--- src/main/java/org/example/conversortemperatura/HelloApplication.java
+++ src/main/java/org/example/conversortemperatura/HelloApplication.java
@@ -11,10 +11,13 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 616, 402);
+        stage.setTitle("Conversor Temperatura Ivan Baguena de la Bandera");
+        stage.resizableProperty().setValue(Boolean.FALSE);
         stage.setScene(scene);
         stage.show();
+        HelloController controller = fxmlLoader.getController();
+        controller.setFecha();
     }
 
     public static void main(String[] args) {
--- src/main/java/org/example/conversortemperatura/HelloController.java
+++ src/main/java/org/example/conversortemperatura/HelloController.java
@@ -1,14 +1,53 @@
 package org.example.conversortemperatura;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+import java.util.Calendar;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Button celsiusButton;
+    @FXML
+    private Button fahrenheitButton;
+    @FXML
+    private Button resetButton;
+
+    @FXML
+    private TextField temperaturaInput;
+    @FXML
+    private TextField temperaturaConvert;
+    @FXML
+    private TextField fecha;
+
+    @FXML
+    private ImageView imagen;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    protected void setFecha() {
+        fecha.setText(Calendar.getInstance().getTime().toString());
+    }
+
+    @FXML
+    protected void onResetButtonClick() {
+        temperaturaInput.clear();
+        temperaturaConvert.clear();
+    }
+
+    @FXML
+    protected void onFahrenheitButtonClick() {
+        double temperaturaBase = Double.parseDouble(temperaturaInput.getText());
+        double temperaturaConv = (temperaturaBase * 9/5) + 32;
+
+        temperaturaConvert.setText(String.valueOf(temperaturaConv));
     }
 }
